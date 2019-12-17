b222ae4bb71d169b196f514389a59988028414b3
shmasf <25368532+shmasf@users.noreply.github.com>
2019-12-17 23:30:32 +0200

    Added a Jupyter noteboob, trying to test the new nbstripout tool.

---
+++ first_notebook.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[11]:
+
+
+arr = np.ndarray(shape=[20], dtype=int)
+display('Array', arr)
+pd.DataFrame(arr, columns=['data'])
+
+
+# In[ ]:
+
+
+
+
